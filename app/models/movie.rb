ce9b2bb350275db9925791c69b901b6846d80408
Ellyn <eka249@nyu.edu>
2019-09-26 10:12:48 -0700

    edited relationships

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ApplicationRecord
-    has_many :list_movies
-    has_many :lists, through: :list_movies
-    has_many :movie_genres
-    has_many :genres, through: :movie_genres
+    has_many :listmovies
+    has_many :lists, through: :listmovies
+    has_many :moviegenres
+    has_many :genres, through: :moviegenres
 
 end
--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,4 +1,4 @@
 class Preference < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
   belongs_to :genre
 end
