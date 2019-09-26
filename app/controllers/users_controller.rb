5e2d428ef74f67d0f7df1479d5bb92a8fa264003
ayerest <irisiseve@gmail.com>
2019-09-25 17:58:22 -0700

    started adding controller actions to the backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render :json users
+    end
+
+    def show
+        user = User.find(params[:id])
+        render :json user
+    end
+
+    def new
+    end
+
+    def create
+        # User.create(username: params[:username], password: params[:password])
+        debugger
+    end
 end
