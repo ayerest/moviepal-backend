528d354e9b51dfd31dd5b8ddfc8f4ce05021c784
ayerest <irisiseve@gmail.com>
2019-10-01 13:19:43 -0700

    authorization skipped for fetch requests

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,4 +1,6 @@
 class MapsController < ApplicationController
+    skip_before_action :authorized, only: [:get_api_key, :get_map]
+
     # before_action :get_params, only: [:get_api_key]
     ## need to add a script tag with ID to target in app.js or index.js
     ## once targetted we can change the script tag's src attribute to the google maps url
@@ -11,7 +13,6 @@ class MapsController < ApplicationController
 
     def get_map
         # binding.pry
-        # byebug
         #need to figure out why params aren't coming through
         ## params from either geolocation (not sure how to do yet) or from event listener on settings form that grabs the city from the user input
         # city = params['city'].gsub(" ","+") 
